11c0955426aa54db97c63ee2c5e8f7d3e151ac35
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-01-15 17:10:48 +0900

    [FIX] #feature/place - clean (제트)

--- src/pages/searchplacepage/searchplace.js
+++ src/pages/searchplacepage/searchplace.js
@@ -4,13 +4,23 @@ import GoogleMapComponent from '../../components/Map.js';
 import SearchComponent from '../../components/Search.js';
 
 class srchplace extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mapLocation: null,
+    };
+  }
+
+  handleSearch = (location) => {
+    // 검색어를 받아와서 지도의 위치를 변경하는 로직을 구현
+    this.setState({ mapLocation: location });
+  };
+
   render() {
     return (
       <div>
-        <SearchComponent />
-
-        <GoogleMapComponent />
-        
+        <SearchComponent onSearch={this.handleSearch} />
+        <GoogleMapComponent location={this.state.mapLocation} />
       </div>
     );
   }
