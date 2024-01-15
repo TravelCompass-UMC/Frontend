ef5b4a61a3975ff4d14ae9c9ba660416664d127a
noeyhgnas <shawn2018@naver.com>
2024-01-15 19:02:44 +0900

    확인 전

--- src/pages/travelplan/travelplan_detail.js
+++ src/pages/travelplan/travelplan_detail.js
@@ -1,20 +1,66 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import SidebarL from "../../components/SidebarL";
+import SidebarR from "../../components/SidebarR";
 import Map from "../../components/Map";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { eachDayOfInterval, format } from "date-fns";
-import "../../styles/travelplan_detail.css";
+
+const renderSidebarContent = (
+  sidebarContent,
+  dates,
+  times,
+  handleTimeChange
+) => {
+  switch (sidebarContent) {
+    case "일정":
+      return dates.map((date) => {
+        const formattedDate = format(date, "yyyy-MM-dd");
+        const { startTime, endTime } = times[formattedDate] || {};
+        return (
+          <div key={formattedDate}>
+            <h3>{format(date, "yyyy년 MM월 dd일")}</h3>
+            <div>
+              <label>시작 시간: </label>
+              <DatePicker
+                selected={startTime}
+                onChange={(date) => handleTimeChange(date, formattedDate, true)}
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="h:mm aa"
+              />
+            </div>
+            <div>
+              <label>종료 시간: </label>
+              <DatePicker
+                selected={endTime}
+                onChange={(date) =>
+                  handleTimeChange(date, formattedDate, false)
+                }
+                showTimeSelect
+                showTimeSelectOnly
+                timeIntervals={15}
+                timeCaption="Time"
+                dateFormat="h:mm aa"
+              />
+            </div>
+          </div>
+        );
+      });
+
+    // 다른 case 처리
+    default:
+      return <p>내용을 선택해 주세요.</p>;
+  }
+};
 
 const ExamplePage = () => {
   const location = useLocation();
   const { startDate, endDate } = location.state || {};
-  const [sidebarContent, setSidebarContent] = useState("일정");
-  const [transportation, setTransportation] = useState("자가용"); // transportation 상태 추가
+  const [sidebarContent, setSidebarContent] = useState("default");
   const [times, setTimes] = useState({});
-  const [hashtag, setHashtag] = useState("");
-  const [hashArr, setHashArr] = useState([]);
 
   useEffect(() => {
     if (startDate && endDate) {
@@ -41,142 +87,22 @@ const ExamplePage = () => {
     }));
   };
 
-  const handleTransportationChange = (mode) => {
-    setTransportation(mode);
-  };
-
-  const onChangeHashtag = useCallback((e) => {
-    setHashtag(e.target.value);
-  }, []);
-
-  const onKeyUp = useCallback(
-    (e) => {
-      if (e.keyCode === 13 && hashtag.trim() !== "") {
-        setHashArr((hashArr) => [...hashArr, hashtag]);
-        setHashtag("");
-      }
-    },
-    [hashtag]
-  );
-
-  // Sidebar의 내용을 설정하는 함수
-  const renderSidebarContent = () => {
-    switch (sidebarContent) {
-      case "일정":
-        return (
-          <div>
-            {eachDayOfInterval({ start: startDate, end: endDate }).map(
-              (date) => {
-                const formattedDate = format(date, "yyyy-MM-dd");
-                const { startTime, endTime } = times[formattedDate] || {};
-                return (
-                  <div key={formattedDate}>
-                    <h3>{format(date, "yyyy년 MM월 dd일")}</h3>
-                    <div>
-                      <label>시작 시간: </label>
-                      <DatePicker
-                        selected={startTime}
-                        onChange={(date) =>
-                          handleTimeChange(date, formattedDate, true)
-                        }
-                        showTimeSelect
-                        showTimeSelectOnly
-                        timeIntervals={15}
-                        timeCaption="Time"
-                        dateFormat="h:mm aa"
-                      />
-                    </div>
-                    <div>
-                      <label>종료 시간: </label>
-                      <DatePicker
-                        selected={endTime}
-                        onChange={(date) =>
-                          handleTimeChange(date, formattedDate, false)
-                        }
-                        showTimeSelect
-                        showTimeSelectOnly
-                        timeIntervals={15}
-                        timeCaption="Time"
-                        dateFormat="h:mm aa"
-                      />
-                    </div>
-                  </div>
-                );
-              }
-            )}
-            <div className="transportation-buttons">
-              <button
-                className={transportation === "자가용" ? "selected" : ""}
-                onClick={() => handleTransportationChange("자가용")}
-              >
-                자가용
-              </button>
-              <button
-                className={transportation === "대중교통" ? "selected" : ""}
-                onClick={() => handleTransportationChange("대중교통")}
-              >
-                대중교통
-              </button>
-            </div>
-            <div className="HashWrap">
-              <div className="HashWrapOuter">
-                {hashArr.map((tag, index) => (
-                  <div key={index} className="HashWrapInner">
-                    #{tag}
-                  </div>
-                ))}
-              </div>
-              <input
-                className="HashInput"
-                type="text"
-                value={hashtag}
-                onChange={onChangeHashtag}
-                onKeyUp={onKeyUp}
-                placeholder="해시태그 입력"
-              />
-            </div>
-          </div>
-        );
-
-      case "숙소":
-        return <p>숙소 선택 내용이 여기에 표시됩니다.</p>;
-
-      case "장소":
-        return <p>장소 선택 내용이 여기에 표시됩니다.</p>;
-
-      default:
-        return <p>내용을 선택해 주세요.</p>;
-    }
-  };
+  const dates =
+    startDate && endDate
+      ? eachDayOfInterval({ start: startDate, end: endDate })
+      : [];
 
   return (
     <div>
-      <SidebarL width={400} isOpen={true}>
-        <button
-          className={sidebarContent === "일정" ? "selected" : ""}
-          onClick={() => setSidebarContent("일정")}
-        >
-          상세일정
-        </button>
-        <button
-          className={sidebarContent === "숙소" ? "selected" : ""}
-          onClick={() => setSidebarContent("숙소")}
-        >
-          숙소선택
-        </button>
-        <button
-          className={sidebarContent === "장소" ? "selected" : ""}
-          onClick={() => setSidebarContent("장소")}
-        >
-          장소선택
-        </button>
-
-        {renderSidebarContent()}
-      </SidebarL>
+      <SidebarR width={600}>
+        <button onClick={() => setSidebarContent("일정")}>상세일정</button>
+        <button onClick={() => setSidebarContent("숙소")}>숙소선택</button>
+        <button onClick={() => setSidebarContent("장소")}>장소선택</button>
+        {renderSidebarContent(sidebarContent, dates, times, handleTimeChange)}
+      </SidebarR>
       <Map />
     </div>
   );
 };
 
 export default ExamplePage;
-
