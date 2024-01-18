7df1263995120fdb99cf7876483ac3b592d53ec4
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-01-19 03:53:12 +0900

    [FIX] #feature/place - map pin (제트)

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,6 +1,6 @@
 // Map.js
 import React, { useState, useEffect } from "react";
-import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const containerStyle = {
   position: "absolute",
@@ -15,15 +15,23 @@ const Center = {
   lng: 128.6014,
 };
 
-const GoogleMapComponent = ({ location }) => {
+const Map = ({ location, onPinClick }) => {
   const [mapCenter, setMapCenter] = useState(Center);
   const [zoomLevel, setZoomLevel] = useState(7);
+  const [markers, setMarkers] = useState([]);
 
   // Update map center and zoom when location changes
   useEffect(() => {
     if (location) {
       setMapCenter(location);
       setZoomLevel(12); // Set a specific zoom level when location is available
+    }
+  }, [location]);
+
+  // Update markers when location changes
+  useEffect(() => {
+    if (location) {
+      setMarkers([{ position: location, label: "1" }]);
     }
   }, [location]);
 
@@ -34,10 +42,20 @@ const GoogleMapComponent = ({ location }) => {
           mapContainerStyle={containerStyle}
           center={location || mapCenter}
           zoom={location ? 12 : zoomLevel}
-        />
+        >
+          {/* Render markers on the map */}
+          {markers.map((marker, index) => (
+            <Marker
+              key={index}
+              position={marker.position}
+              label={marker.label}
+              onClick={() => onPinClick(`Place ${marker.label}`)}
+            />
+          ))}
+        </GoogleMap>
       </LoadScript>
     </div>
   );
 };
 
-export default GoogleMapComponent;
+export default Map;
