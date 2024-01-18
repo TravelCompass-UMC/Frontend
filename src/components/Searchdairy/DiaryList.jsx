13ead37aab2f4ff8e417d457a34637faf7f187a3
sondahyun <88223753+sondahyun@users.noreply.github.com>
2024-01-19 01:45:14 +0900

    [ADD]

--- src/components/Searchdairy/DiaryList.jsx
+++ src/components/Searchdairy/DiaryList.jsx
@@ -102,6 +102,22 @@ const contents = [
   // 추가적인 데이터는 여기에 계속 추가
 ];
 
+const styles = {
+  contextBox: {
+    color: "#191B24",
+    fontSize: 22,
+    fontFamily: "SUIT Variable",
+    fontWeight: "700",
+    wordWrap: "break-word",
+  },
+  imageTitle: {
+    width: "50px",
+    height: "30px",
+    opacity: 0.6,
+    border: "6px #8270FF solid",
+  },
+};
+
 function DiaryList(props) {
   const itemsPerRow = 3; // Number of diaries per row
   const rowsPerPage = 3; // Number of rows per page
@@ -121,51 +137,65 @@ function DiaryList(props) {
   };
 
   return (
-    <div>
-      {Array.from({ length: rowsPerPage }).map((_, rowIndex) => (
-        <div
-          key={rowIndex}
-          style={{ display: "flex", justifyContent: "center", gap: "20px" }}
-        >
-          {currentItems
-            .slice(rowIndex * itemsPerRow, (rowIndex + 1) * itemsPerRow)
-            .map((place, index) => (
-              <Diary key={index} place={place.place} hashtag={place.hashtag} />
-            ))}
-        </div>
-      ))}
-
-      <div
-        style={{ display: "flex", justifyContent: "center", marginTop: "20px" }}
-      >
-        <button
-          onClick={() => handlePageChange(currentPage - 1)}
-          disabled={currentPage === 1}
-        >
-          이전 페이지
-        </button>
-
-        {Array.from({ length: totalPages }).map((_, index) => (
-          <button
-            key={index}
-            onClick={() => handlePageChange(index + 1)}
-            style={{
-              margin: "0 5px",
-              fontWeight: index + 1 === currentPage ? "bold" : "normal",
-            }}
+    <>
+      <div style={styles.contextBox}>
+        {/* <div style={styles.imageTitle}></div>계획 둘러보기 */}
+        <img src={"../../assets/images/Pages/Vetor.png"} alt="vector" />
+      </div>
+      <div>
+        {Array.from({ length: rowsPerPage }).map((_, rowIndex) => (
+          <div
+            key={rowIndex}
+            style={{ display: "flex", justifyContent: "center", gap: "20px" }}
           >
-            {index + 1}
-          </button>
+            {currentItems
+              .slice(rowIndex * itemsPerRow, (rowIndex + 1) * itemsPerRow)
+              .map((place, index) => (
+                <Diary
+                  key={index}
+                  place={place.place}
+                  hashtag={place.hashtag}
+                />
+              ))}
+          </div>
         ))}
 
-        <button
-          onClick={() => handlePageChange(currentPage + 1)}
-          disabled={currentPage === totalPages}
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            marginTop: "20px",
+          }}
         >
-          다음 페이지
-        </button>
+          <button
+            onClick={() => handlePageChange(currentPage - 1)}
+            disabled={currentPage === 1}
+          >
+            이전 페이지
+          </button>
+
+          {Array.from({ length: totalPages }).map((_, index) => (
+            <button
+              key={index}
+              onClick={() => handlePageChange(index + 1)}
+              style={{
+                margin: "0 5px",
+                fontWeight: index + 1 === currentPage ? "bold" : "normal",
+              }}
+            >
+              {index + 1}
+            </button>
+          ))}
+
+          <button
+            onClick={() => handlePageChange(currentPage + 1)}
+            disabled={currentPage === totalPages}
+          >
+            다음 페이지
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
