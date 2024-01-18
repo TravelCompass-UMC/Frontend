949c2b213a448d1bc02084f2608e0f403cedc145
topeanut <ljhh1648@gmail.com>
2024-01-18 17:00:11 +0900

    temp

--- src/pages/searchplacepage/placeinfo.js
+++ src/pages/searchplacepage/placeinfo.js
@@ -1,11 +1,31 @@
 // placeinfo.js
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
 import SidebarL from "../../components/SidebarL";
+import GoogleMapComponent from "../../components/Map.js";
 
 const PlaceInfo = () => {
   const { query } = useParams();
+  const [mapLocation, setMapLocation] = useState(null);
+
+  useEffect(() => {
+    // 검색어를 사용하여 지도 위치 가져오기
+    const fetchLocation = async () => {
+      try {
+        const geocodingApiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=YOUR_API_KEY`;
+        const response = await fetch(geocodingApiUrl);
+        const data = await response.json();
+        const location = data.results[0]?.geometry?.location;
+        if (location) {
+          setMapLocation(location);
+        }
+      } catch (error) {
+        console.error("Error fetching geocoding data:", error);
+      }
+    };
+
+    fetchLocation();
+  }, [query]);
 
   return (
     <div>
@@ -14,16 +34,20 @@ const PlaceInfo = () => {
         <p>대한민국</p>
         <p>지금 대한민국의 가장 인기 많은 지역</p>
         <ul>
-          <li>TOP1 <br/>
+          <li>
+            TOP1 <br />
             <Link to={`/city/seoul`}>서울</Link>
           </li>
-          <li>TOP2 <br/>
+          <li>
+            TOP2 <br />
             <Link to={`/city/jeju`}>제주도</Link>
           </li>
-          <li>TOP3 <br/>
+          <li>
+            TOP3 <br />
             <Link to={`/city/busan`}>부산</Link>
           </li>
-          <li>TOP4 <br/>
+          <li>
+            TOP4 <br />
             <Link to={`/city/gyeongju`}>경주</Link>
           </li>
         </ul>
@@ -31,6 +55,7 @@ const PlaceInfo = () => {
           <p>선택하신 도시의 가장 인기 많은 장소</p>
         </div>
       </SidebarL>
+      <GoogleMapComponent location={mapLocation} />
     </div>
   );
 };
--- src/pages/searchplacepage/searchplace.js
+++ src/pages/searchplacepage/searchplace.js
@@ -1,29 +1,25 @@
-import React, { Component } from "react";
-import "../../styles/Place.css"; 
-import GoogleMapComponent from '../../components/Map.js';
-import SearchComponent from '../../components/Search.js';
+// srchplace.js
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../../styles/Place.css";
+import GoogleMapComponent from "../../components/Map.js";
+import SearchComponent from "../../components/Search.js";
 
-class srchplace extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mapLocation: null,
-    };
-  }
+const SrchPlace = () => {
+  const [mapLocation, setMapLocation] = useState(null);
+  const navigate = useNavigate();
 
-  handleSearch = (location) => {
-    // 검색어를 받아와서 지도의 위치를 변경하는 로직을 구현
-    this.setState({ mapLocation: location });
+  const handleSearch = (location, searchQuery) => {
+    setMapLocation(location);
+    navigate(`/placeinfo/${searchQuery}`); // 검색어를 URL에 포함하여 네비게이션
   };
 
-  render() {
-    return (
-      <div>
-        <SearchComponent onSearch={this.handleSearch} />
-        <GoogleMapComponent location={this.state.mapLocation} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <SearchComponent onSearch={handleSearch} />
+      <GoogleMapComponent location={mapLocation} />
+    </div>
+  );
+};
 
-export default srchplace;
+export default SrchPlace;
