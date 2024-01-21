a58ef562ca45922b8ffa74cbc945720c9bfe97f4
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-01-22 03:12:25 +0900

    [ADD] #feature/place - recommend card (제트)

--- src/api/MapDetail.js
+++ src/api/MapDetail.js
@@ -1,37 +1,22 @@
-// src>api>MapDetail.js
-export const getPlaceDetails = async ({ placeName }) => {
-  const apiKey = "AIzaSyAxcBF_X0UjuYxGNAxZ2pNrQSDyL4AyS4U";
-  
+// src/api/MapDetail.js
+export const getPlaceDetails = async ({ placeId }) => {
+  const apiKey = "AIzaSyAxcBF_X0UjuYxGNAxZ2pNrQSDyL4AyS4U"; // Replace with your actual API key
+
   try {
-    const response = await fetch(
-      `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${placeName}&inputtype=textquery&fields=place_id&key=${apiKey}`
+    const detailsResponse = await fetch(
+      `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`
     );
 
-    if (!response.ok) {
+    if (!detailsResponse.ok) {
       throw new Error('Failed to fetch place details');
     }
 
-    const data = await response.json();
+    const detailsData = await detailsResponse.json();
 
-    if (data.status === 'OK' && data.candidates && data.candidates.length > 0) {
-      const placeId = data.candidates[0].place_id;
-      const detailsResponse = await fetch(
-        `https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${apiKey}`
-      );
-
-      if (!detailsResponse.ok) {
-        throw new Error('Failed to fetch place details');
-      }
-
-      const detailsData = await detailsResponse.json();
-
-      if (detailsData.status === 'OK') {
-        return detailsData.result;
-      } else {
-        throw new Error('Failed to fetch place details');
-      }
+    if (detailsData.status === 'OK') {
+      return detailsData.result;
     } else {
-      throw new Error('Failed to find place ID');
+      throw new Error('Failed to fetch place details');
     }
   } catch (error) {
     console.error('Error fetching place details:', error);
