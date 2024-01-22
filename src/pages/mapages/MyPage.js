8ec30d7a657481dd88a2f08197897aa334311798
noeyhgnas <shawn2018@naver.com>
2024-01-22 22:59:09 +0900

    썸네일까지 어느정도?

--- src/pages/mapages/MyPage.js
+++ src/pages/mapages/MyPage.js
@@ -6,17 +6,57 @@ import styles from "../../styles/Mypages.css";
 import { Container } from "react-bootstrap";
 import myplans from '../../tempdata/myplandata';
 import { useState } from "react";
+import otherplans from "../../tempdata/otherplandata";
+
+
+const Thumbnail = {
+  bigBox: {
+    width: "300px",
+    height: "210px",
+    background: "white",
+    borderRadius: 16,
+    border: "1px #EBEDF8 solid",
+    marginBottom: "20px",
+  },
+  imageBox: {
+    width: "300px",
+    height: "120px",
+    background: "#EBEDF8",
+    borderRadius: 15,
+  },
+  textBox: {
+    width: "300px",
+    height: "80px",
+    // background: "#FFFFFF",
+    borderRadius: 15,
+  },
+  placeText: {
+    color: "#191B24",
+    fontSize: 17,
+    fontFamily: "SUIT Variable",
+    fontWeight: "700",
+    wordWrap: "break-word",
+  },
+  hashtagText: {
+    color: "#7F85A3",
+    fontSize: 13,
+    fontFamily: "SUIT Variable",
+    fontWeight: "400",
+    // lineHeight: 25.5,
+    wordWrap: "break-word",
+  },
+};
 
 function Mypage() {
 
     const [plans] = useState(myplans);
-
+    const [others] = useState(otherplans);
 
     return (
       <div className="container">
         <ProfileSection />
         <MyTravelPlanSection title="나의 여행계획" plans={plans}/>
-        <OtherTravelPlanSection title="관심있는 여행계획"/>
+        <OtherTravelPlanSection title="관심있는 여행계획" others={others}/>
         <InterestedPlacesSection title="관심있는 장소" />
         {/* <p>{plans[0].place}</p> */}
         <div className="log-botton">
@@ -45,10 +85,10 @@ export function ProfileSection() {
 
 export function MyplanThumbnail(props){
   return (
-    <div className="col-md-4">
-      <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
-      <h4>{props.plans.place}</h4>
-      <p>{props.plans.hashtag.join(', ')}</p>
+    <div className="col-md-4" style={{...Thumbnail.bigBox, margin: '0 10px'}} >
+      <img style={{...Thumbnail.imageBox, width: '100%'}} src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
+      <a style={Thumbnail.placeText}>{props.plans.place}</a>
+      <p style={Thumbnail.hashtagText}>{props.plans.hashtag.join(', ')}</p>
     </div>
   )
 }
@@ -78,13 +118,16 @@ export function OtherplanThumbnail(props){
   return (
     <div className="col-md-4">
       <img src={'https://codingapple1.github.io/shop/shoes' + props.i + '.jpg'} width="80%" />
-      <h4>{props.plans.place}</h4>
-      <p>{props.plans.hashtag.join(', ')}</p>
+      <h4>{props.others.place}</h4>
+      <p>{props.others.hashtag.join(', ')}</p>
     </div>
   )
 }
 
-export function OtherTravelPlanSection({ title }) {
+export function OtherTravelPlanSection({ title, others }) {
+  // 처음 3개의 요소만 사용
+  const firstThreeOthers = others.slice(0, 3);
+
   return (
     <div className="w-full mb-8">
       <div className="nav-bar" style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
@@ -92,10 +135,24 @@ export function OtherTravelPlanSection({ title }) {
         <Link to="/otherplan" className="morebutton"><button>더보기</button></Link>
       </div>
       <div className="row">
-        
-        </div>
+        {
+          firstThreeOthers.map((other, i) => (
+            <OtherplanThumbnail key={i} others={other} i={i + 1}></OtherplanThumbnail>
+          ))
+        }
+      </div>
     </div>
   );
+}
+
+export function InteresetedPlaceThumbnail(props){
+  return (
+    <div className="col-md-4">
+      <img src={''} alt="장소이미지" width="80%" />
+      <h4>{props.others.place}</h4>
+      <p>{props.others.hashtag.join(', ')}</p>
+    </div>
+  )
 }
 
 export function InterestedPlacesSection({title}) {
--- src/pages/mapages/Otherplanpage.js
+++ src/pages/mapages/Otherplanpage.js
@@ -1,17 +1,32 @@
+//Otherplanpage.js
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import styles from "../../styles/Mypages.css";
+import { OtherTravelPlanSection } from "./MyPage";
+import { OtherplanThumbnail } from "./MyPage";
+import { Container } from "react-bootstrap";
+import { useState } from "react";
+import otherplans from "../../tempdata/otherplandata";
 
-class Otherplanpage extends Component {
-    render() {
-      return (
-        <div>
-            관심있는 여행계획서 페이지
-            
-        </div>
-  
-      );
-    }
+function Otherplanpage() {
+
+  const [others] = useState(otherplans);
+
+  return (
+    <div className="container">
+      <h3>관심있는 여행계획</h3>
+      <div className="row">
+        {
+          others.map((a, i) => {
+            return (
+              <OtherplanThumbnail others={others[i]} i={i + 1}></OtherplanThumbnail>
+            )
+          })
+        }
+      </div>
+    </div>
+  );
+
   }
   
   export default Otherplanpage;
