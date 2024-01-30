5a359e6798f05728311cebc5e312969b99cf3a17
topeanut <ljhh1648@gmail.com>
2024-01-31 02:06:53 +0900

    lgin lee

--- src/components/lginModal.js
+++ src/components/lginModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "../styles/LginModal.module.css";
 import MainLogo from "../assets/images/Mypage/Mainlogo.svg";
 import NaverLogo from "../assets/images/Mypage/NaverLogo.svg";
@@ -6,9 +6,40 @@ import NaverLogo from "../assets/images/Mypage/NaverLogo.svg";
 const Modal = ({ open, close }) => {
   // 네이버 로그인 버튼 클릭 핸들러
   const handleNaverLogin = () => {
-    // 백엔드로 요청을 보내는 URI로 리다이렉트
-    window.location.href = "http://localhost:8080/oauth2/authorization/naver";
+    window.location.href =
+      "https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=3tVKSO15tNGbkeZJf8eE&scope=nickname%20email%20profile_image&state=2sByuLo9Si0KbSG2_8jeHXLXSspYMV7N4MmdmWEvG2w%3D&redirect_uri=http://dev.enble.site/login/oauth2/code/naver";
   };
+
+  // 콜백 URL에서 code와 state 추출
+  const getCodeAndStateFromCallback = () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const code = urlParams.get("code");
+    const state = urlParams.get("state");
+
+    if (code && state) {
+      requestAccessToken(code, state);
+    }
+  };
+
+  // 액세스 토큰 요청
+  const requestAccessToken = (code, state) => {
+    const tokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=3tVKSO15tNGbkeZJf8eE&client_secret=zHvANLwWHH&code=${code}&state=${state}`;
+
+    // 실제 요청 로직은 서버 사이드에서 수행되어야 함
+    // 여기서는 예시로만 작성
+    fetch(tokenUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Access Token:", data);
+        // 토큰 처리 로직 추가
+      })
+      .catch((error) => console.error("Error fetching access token:", error));
+  };
+
+  // 컴포넌트가 마운트될 때 콜백 URL 체크
+  useEffect(() => {
+    getCodeAndStateFromCallback();
+  }, []);
 
   return (
     <div
