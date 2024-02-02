2e7cb4f5779bb02e8ba502f6280152221ced2185
topeanut <ljhh1648@gmail.com>
2024-02-03 02:38:35 +0900

    sidebar edit lee

--- src/components/SidebarL.js
+++ src/components/SidebarL.js
@@ -1,53 +1,43 @@
-import React, { useEffect, useRef, useState, useCallback } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "../styles/sidebarL.css";
 
-const SidebarL = ({ width = 280, children }) => {
-  const [isOpen, setOpen] = useState(true); // 초기 상태를 열림으로 설정
-  const [xPosition, setX] = useState(0); // 초기 xPosition 값 0으로 설정하여 열린 상태로 시작
+const SidebarL = ({
+  width = 280,
+  children,
+  isOpen: externalIsOpen,
+  toggleSidebar,
+}) => {
+  const [internalIsOpen, setInternalIsOpen] = useState(true);
+  const isOpen = externalIsOpen !== undefined ? externalIsOpen : internalIsOpen;
+  const xPosition = isOpen ? 0 : -width;
   const side = useRef();
 
-  const toggleMenu = () => {
-    setOpen(!isOpen); // 상태를 토글하는 것으로 변경
+  // External toggle function is not provided, use internal state
+  const handleToggle = () => {
+    if (toggleSidebar) {
+      toggleSidebar();
+    } else {
+      setInternalIsOpen(!internalIsOpen);
+    }
   };
 
-  const handleClose = useCallback(
-    (e) => {
-      if (isOpen && !side.current.contains(e.target)) {
-        setX(-width);
-        setOpen(false);
-      }
-    },
-    [isOpen, width]
-  ); // 의존성 배열에 isOpen과 width 추가
-
   useEffect(() => {
-    window.addEventListener("click", handleClose);
-    return () => {
-      window.removeEventListener("click", handleClose);
-    };
-  }, [handleClose]); // handleClose 함수를 의존성 배열에 추가
-
-  // isOpen 상태에 따라 xPosition 값 설정
-  useEffect(() => {
-    setX(isOpen ? 0 : -width);
-  }, [isOpen, width]);
+    if (externalIsOpen !== undefined && externalIsOpen !== internalIsOpen) {
+      setInternalIsOpen(externalIsOpen);
+    }
+  }, [externalIsOpen, internalIsOpen]);
 
   return (
     <div className="container">
       <div
         ref={side}
         className="sidebar"
-        style={{
-          transform: `translateX(${xPosition}px)`,
-          width: `${width}px`,
-        }}
+        style={{ transform: `translateX(${xPosition}px)`, width: `${width}px` }}
       >
         <button
-          onClick={toggleMenu}
+          onClick={handleToggle}
           className="toggle-button"
-          style={{
-            transform: `translate(${width}px, 20px)`,
-          }}
+          style={{ transform: `translate(${width}px, 20px)` }}
         >
           {isOpen ? "<" : ">"}
         </button>
