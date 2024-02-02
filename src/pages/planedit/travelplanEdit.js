3a59e21948b9f422431bf07402ce9705251987a6
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-02-02 13:47:51 +0900

    [ADD] #feature/edit - edit page (제트)

---
+++ src/pages/planedit/travelplanEdit.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DiaryContent from "../../components/Searchdairy/DiaryContent";
+
+export default function diarycontent() {
+  return (
+    <div style={{ width: "100%", height: "94vh", background: "#F3F5FF" }}>
+      <DiaryContent />
+    </div>
+  );
+}
