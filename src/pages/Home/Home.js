1d6e52e4a2669fbaf7bfd2dc6c7e98d8d45ba7e6
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-02-04 13:50:32 +0900

    [FIX] #feature/edit - issue (제트)

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,16 +3,15 @@ import { Link } from "react-router-dom";
 import '../../styles/Home.css';
 import GoogleMapComponent from "../../components/Map";
 import SidebarR from "../../components/SidebarR";
-import SidebarL from "../../components/SidebarL";
 
 class home extends Component {
   render() {
     return (
-      <div className="container">
-        <div className="map">배경 이미지 지도</div>
+      <div>
+        <div className="map">
         <GoogleMapComponent/>
+        </div>
         <input type="text" className="searchbox" placeholder="궁금한 지역을 검색해보세요"></input>
-        <SidebarL width={320}></SidebarL>
         <SidebarR width={320}>
         <h1>대한민국</h1>
         <p>지금 대한민국의 가장 인기 많은 지역</p>
