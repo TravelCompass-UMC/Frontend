6425c7846fb7052d36e9b622452fb965179b04fb
syddl0 <137189866+shroqkf@users.noreply.github.com>
2024-02-20 02:21:18 +0900

    [FIX] #feature/edit - issue (제트)

--- src/components/planedit/SearchRecommendations.jsx
+++ src/components/planedit/SearchRecommendations.jsx
@@ -2,29 +2,36 @@ import React from 'react';
 import styles from '../../styles/planedit/Recommendation.module.css';
 
 
-// SearchRecommendations.jsx
 const SearchRecommendations = ({ places, onRecommendationClick }) => {
+
+  // Function to generate photo URL for a given photo reference
   const getPhotoUrl = (photoReference) => {
     const apiKey = 'AIzaSyBPG58Nk2zPjucy4apqdFTrUxZl0bGpddU'; // 실제 API 키 사용
     return `https://maps.googleapis.com/maps/api/place/photo?maxwidth=300&photoreference=${photoReference}&key=${apiKey}`;
   };
 
   return (
+    
     <div className={styles.recommendationsContainer}>
       {places.map((place, index) => (
-        <div key={index} className={styles.recommendationItem} onClick={() => onRecommendationClick(place)}>
+        <div 
+          key={index} 
+          className={styles.recommendationItem}
+          onClick={() => onRecommendationClick(place)}
+        >
           <div className={styles.recommendationImage}>
-            <img src={place.photos.length > 0 ? getPhotoUrl(place.photos[0]) : '기본_이미지_경로'} alt={place.name} />
+            <img src={place.photos ? getPhotoUrl(place.photos[0]) : 'path_to_default_image'} alt={place.name} />
           </div>
           <div className={styles.recommendationInfo}>
             <h3 className={styles.recommendationTitle}>{place.name}</h3>
-            <p className={styles.recommendationDescription}>{place.vicinity}</p>
+            <p className={styles.recommendationDescription}>
+              {place.vicinity || "위치를 알 수 없습니다."} 
+            </p>
           </div>
         </div>
       ))}
     </div>
   );
 };
-
 
 export default SearchRecommendations;
